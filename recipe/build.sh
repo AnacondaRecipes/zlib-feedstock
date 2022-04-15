527714f2cf696fe4947148f42c93f101cbaeb2cb
Scott Bailey <sbailey@anaconda.com>
2022-04-15 16:32:09 -0700

    return to cmake for aarch64

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,41 +12,27 @@ export CFLAGS="${CFLAGS} -fPIC"
 export CXXFLAGS="${CXXFLAGS} -fPIC"
 
 
-if [[ ${target_platform} == linux-aarch64 ]]; then
-
-  echo "Configuring..."
-  ./configure --prefix=${PREFIX} --shared
-
-  echo "Building..."
-  make -j${CPU_COUNT} ${VERBOSE_AT}
-  make check
-
-else
-
-  # Isolate the build.
-  mkdir -p Build
-  cd Build || exit 1
+# Isolate the build.
+mkdir -p Build
+cd Build || exit 1
 
 
-
-  # Generate the build files.
-  echo "Generating the build files."
-  cmake .. ${CMAKE_ARGS} \
+# Generate the build files.
+echo "Generating the build files."
+cmake .. ${CMAKE_ARGS} \
       -G"Unix Makefiles" \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
 
-  # Build.
-  echo "Building..."
-  make  -j${CPU_COUNT} || exit 1
+# Build.
+echo "Building..."
+make  -j${CPU_COUNT} || exit 1
 
 
-  # Perform tests.
-  echo "Testing..."
-  ctest -VV --output-on-failure || exit 1
-
-fi
+# Perform tests.
+echo "Testing..."
+ctest -VV --output-on-failure || exit 1
 
 
 # Installing
