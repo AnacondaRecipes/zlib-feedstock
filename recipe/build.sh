6263b7e732163895ff6a93cf02953c5c2124e298
scott hain <shain@anaconda.com>
2024-11-13 05:56:51 -0800

    Cleaned up build script, verified on osx, moving outwards now
    Signed-off-by: scott hain <shain@anaconda.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,38 +1,10 @@
 #!/bin/bash
 
-###########################################################################
-# THIS WILL PROBABLY BREAK EVERYTHING
-#TODO: FIX THIS
-export CFLAGS="${CFLAGS} -fPIC"
-export CXXFLAGS="${CXXFLAGS} -fPIC"
-
-./configure --prefix=${PREFIX}  \
-    --shared || (cat configure.log && false)
-
-make -j${CPU_COUNT} ${VERBOSE_AT}
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
-    make check
-fi
-make install
-
-# Remove man files.
-rm -rf $PREFIX/share
-
-# Copy license file to the source directory so conda-build can find it.
-cp $RECIPE_DIR/license.txt $SRC_DIR/license.txt
-#################################################################################
-
-# # Set flags
-# export CFLAGS=$(echo ${CFLAGS} | sed 's|-O2|-O3|g')
-# export CPPFLAGS=$(echo ${CPPFLAGS} | sed 's|-O2|-O3|g')
-
+#TODO: This may not be needed
 # MACH=$(${CC} -dumpmachine)
 # if [[ ${MACH} =~ x86_64.* ]] || [[ ${MACH} =~ i?86.* ]]; then
 #   export CFLAGS="${CFLAGS} -DUNALIGNED_OK"
 # fi
-# export CFLAGS="${CFLAGS} -fPIC"
-# export CXXFLAGS="${CXXFLAGS} -fPIC"
-
 # # linux-aarch64 activations fails to set `ar` tool. This can be
 # # removed when activations is corrected.
 # if [[ "${target_platform}" == linux-aarch64 ]]; then
@@ -41,10 +13,10 @@ cp $RECIPE_DIR/license.txt $SRC_DIR/license.txt
 #   fi
 # fi
 
+#TODO: AVOID IF AT ALL POSSIBLE, IDEALLY REMOVE CMAKE DEP
 # # Isolate the build.
 # mkdir -p Build
 # cd Build || exit 1
-
 
 # # Generate the build files.
 # echo "Generating the build files."
@@ -54,27 +26,37 @@ cp $RECIPE_DIR/license.txt $SRC_DIR/license.txt
 #       -DCMAKE_INSTALL_PREFIX=$PREFIX \
 #       -DCMAKE_BUILD_TYPE=Release \
 
-# # Build.
-# echo "Building..."
-# make  -j${CPU_COUNT} || exit 1
 
+export CFLAGS="${CFLAGS} -fPIC"
+export CXXFLAGS="${CXXFLAGS} -fPIC"
 
-# # Perform tests.
-# echo "Testing..."
-# ctest -VV --output-on-failure || exit 1
+./configure --prefix=${PREFIX}  \
+    --shared || (cat configure.log && false)
 
+# Build.
+echo "Building..."
+make -j${CPU_COUNT} ${VERBOSE_AT} || exit 1
 
-# # Installing
-# echo "Installing..."
-# make install || exit 1
+# Perform tests.
+echo "Testing..."
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+    make check
+fi
+#TODO: This maybe should be in the previous block
+ctest -VV --output-on-failure || exit 1
 
-# # Remove man files.
-# rm -rf $PREFIX/share
+# Installing
+echo "Installing..."
+make install || exit 1
+make install
 
-# # Copy license file to the source directory so conda-build can find it.
-# cp $RECIPE_DIR/license.txt $SRC_DIR/license.txt
+# Remove man files.
+rm -rf $PREFIX/share
 
+# Copy license file to the source directory so conda-build can find it.
+cp $RECIPE_DIR/license.txt $SRC_DIR/license.txt
 
-# # Error free exit!
-# echo "Error free exit!"
-# exit 0
+# Error free exit!
+echo "Error free exit!"
+exit 0
+
